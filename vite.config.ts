f4e80771f51f582bf1096c7162862655a936e9b7
Miguel Montesinos <miguel.montesinos@zeusbyllyc.com>
2025-05-13 14:53:57 +0200

    ref: src/ to lib/

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import dts from 'vite-plugin-dts';
 export default defineConfig({
   build: {
     lib: {
-      entry: 'src/index.ts',
+      entry: 'lib/index.ts',
       fileName: (format) => `types-storage.${format === 'es' ? 'js' : 'cjs'}`,
       formats: ['es', 'cjs'],
     },
