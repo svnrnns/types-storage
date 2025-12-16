2bd04fe4c6266ed45110292f5c66f5137a7932eb
Miguel <migueldeveloper@lioncapitalg.com>
2025-12-16 15:44:21 +0100

    chore: updated vite compiler to vite standards

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,21 @@
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
+const PACKAGE_NAME = 'types-storage';
+
 export default defineConfig({
-  build: {
-    lib: {
-      entry: 'lib/index.ts',
-      fileName: (format) => `types-storage.${format === 'es' ? 'js' : 'cjs'}`,
-      formats: ['es', 'cjs'],
-    },
-  },
   plugins: [
     dts({
       insertTypesEntry: true,
       outDir: 'dist',
-      entryRoot: 'lib',
+      entryRoot: 'src',
     }),
   ],
-  test: {
-    environment: 'happy-dom',
-    globals: true,
-    setupFiles: ['./tests/setup.ts'],
+  build: {
+    lib: {
+      entry: 'lib/index.ts',
+      fileName: (format) => `${PACKAGE_NAME}.${format === 'es' ? 'js' : 'cjs'}`,
+      formats: ['es', 'cjs'],
+    },
   },
 });
